626668231a6baddd05fe3d028560b0ed9cbb3738
egecitak <egecitak01@gmail.com>
2021-10-21 13:54:46 +0300

    18th October 21- End of Class

---
+++ src/app.js
@@ -0,0 +1,21 @@
+console.log("Hello World")
+
+//JS isn't type safe.
+let dolarDun = 9.20
+let dolarBugun = 9.30
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+//euroDun = 11
+
+console.log(euroDun)
+
+let konutKredileri = ["Konut Kredisi", "Emlak Konut Kredisi", "Kamu Konut Kredisi"]
+
+for (let i = 0; i < konutKredileri.length; i++) {
+    console.log(konutKredileri[i])
+}
+
+console.log(konutKredileri)
